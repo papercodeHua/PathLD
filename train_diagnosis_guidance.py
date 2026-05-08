e5ddb15dc3682dccd7b8600b080d7420e8b7fd21
papercodeHua <37338375+papercodeHua@users.noreply.github.com>
2026-05-08 11:19:05 +0800

    Update train_diagnosis_guidance.py

--- train_diagnosis_guidance.py
+++ train_diagnosis_guidance.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 from sklearn.metrics import accuracy_score
 from utils.utils import seed_torch, save_checkpoint, load_checkpoint, write_csv_header
 from utils.config import config
-from model.da_net import DA_NET3D  
+from model.diagnosis_guidance_net import DA_NET3D
 from dataset.adni_dataset import ThreeClassDataset
 
 import warnings
