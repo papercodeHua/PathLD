f67c884bb3220db1336c8a27aecacd35363a1736
papercodeHua <37338375+papercodeHua@users.noreply.github.com>
2026-05-08 11:08:51 +0800

    Update unet.py

--- model/unet.py
+++ model/unet.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 from utils.config import config
 from inspect import isfunction
-from model.pf_blocks import GroupNorm, Upsample, Downsample, ResidualBlock, Swish, PFBlock 
+from model.pf_block import GroupNorm, Upsample, Downsample, ResidualBlock, Swish, PFBlock 
 import nibabel as nib
 import numpy as np
 import os
